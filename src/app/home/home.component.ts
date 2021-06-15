45c8cf91937c205b02f4a3bfbf7839a2ea86acf0
Waqas <wakassadik@gmail.com>
2021-06-15 16:52:44 +0500

    creating and destroying a custome 100% obserablle

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 
-import { interval, Subscription } from 'rxjs';
+import { interval, Subscription, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -11,6 +11,7 @@ export class HomeComponent implements OnInit, OnDestroy {
   // when using custom subscription, need to store the subscription to later destroy it
   // otherwise, it would keep on running in the background, even if you are no more on that page.
   private firstObsSubscription: Subscription;
+  private customeSubscription: Subscription;
 
   constructor() { }
   
@@ -19,13 +20,33 @@ export class HomeComponent implements OnInit, OnDestroy {
     // writing our own obserable
     this.firstObsSubscription = interval(1000).subscribe(count => {
       console.log(count);
-    })
+    });
+
+    // writing pure obserablle
+    const customIntervalObserable = Observable.create( observer => {
+      let count = 100;
+      setInterval( () =>{
+        observer.next(count);
+        count += 100;
+        // to throw an error
+        //observer.error();
+
+        // to tell that you are done
+        //observer.complete();
+
+
+      }, 1000);
+    });
+
+    this.customeSubscription = customIntervalObserable.subscribe(data => {
+        console.log("custom count: " + data);
+    });
   }
 
   ngOnDestroy(): void {
     // it is to prevent memory leaks.
     this.firstObsSubscription.unsubscribe();
-    
+    this.customeSubscription.unsubscribe();
   }
 
 }
