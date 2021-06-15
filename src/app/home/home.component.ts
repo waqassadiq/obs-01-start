bb882f2822d6834f00e6a48182a005e5e2926354
Waqas <wakassadik@gmail.com>
2021-06-15 16:20:00 +0500

    adding a custom obserablel and destroying it

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+
+import { interval, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
+  // when using custom subscription, need to store the subscription to later destroy it
+  // otherwise, it would keep on running in the background, even if you are no more on that page.
+  private firstObsSubscription: Subscription;
 
   constructor() { }
+  
 
   ngOnInit() {
+    // writing our own obserable
+    this.firstObsSubscription = interval(1000).subscribe(count => {
+      console.log(count);
+    })
+  }
+
+  ngOnDestroy(): void {
+    // it is to prevent memory leaks.
+    this.firstObsSubscription.unsubscribe();
+    
   }
 
 }
