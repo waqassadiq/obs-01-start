e2d9faf419a355b6775d7e5022b44160f6dad41f
Waqas <wakassadik@gmail.com>
2021-06-16 12:45:17 +0500

    using eventEmiiter to activate a section of the code via service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from './user.service';
 
 @Component({
   selector: 'app-root',
@@ -6,7 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  constructor() {}
+  constructor(private userService: UserService) {}
 
-  ngOnInit() {}
+  userActiviated: boolean;
+  ngOnInit() {
+    this.userService.activatedEmitter.subscribe(didActivate =>{
+      this.userActiviated = didActivate;
+    });
+  }
 }
---
+++ src/app/user.service.ts
@@ -0,0 +1,6 @@
+import { Injectable, EventEmitter } from "@angular/core";
+
+@Injectable({providedIn: 'root'})
+export class UserService{
+    activatedEmitter = new EventEmitter<boolean>();
+}
